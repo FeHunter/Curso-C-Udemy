bb01d08921df6f84150709ac28d5437b0df325dd
Felipe <41969682+FeHunter@users.noreply.github.com>
2022-11-02 21:45:09 -0300

    Add files via upload

---
+++ 08-Altura_Media_Array/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace Programa {
+    class Program {
+        static void Main (){
+
+            int n = int.Parse(Console.ReadLine());
+
+            double[] vect = new double[n];
+
+            for (int i = 0; i < n; i ++){
+                vect[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+
+            double sum = 0;
+            for (int i =0; i < n; i++){
+                sum += vect[i];
+            }
+
+            double avg = sum / n;
+
+            Console.WriteLine ("Average Hight = "+ avg.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
