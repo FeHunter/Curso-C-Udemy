6c9f3972ea5d9769e56995776b5d184ffd143e3e
Felipe <41969682+FeHunter@users.noreply.github.com>
2022-11-02 21:32:19 -0300

    Add files via upload
    Calculate a ray values

---
+++ 03-CalculadoraRaio/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace CalculadoraRaio {
+    class Program {
+        static void Main (string[] args){
+
+            Console.WriteLine();
+            Console.Write("Entre o valor do raio: ");
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double circ = Calculadora.Circunferencia(raio);
+            double volume = Calculadora.Volume(raio);
+
+            Console.WriteLine("Circunferencia: "+ circ.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Volume: "+ volume.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Pi: "+ Calculadora.Pi.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
